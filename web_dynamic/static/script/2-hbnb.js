7e3c57c779c0149d3f83c3cfa740587497c21716
Y-admen <yassminash305@gmail.com>
2024-06-01 12:26:05 +0300

    update

--- web_dynamic/static/script/2-hbnb.js
+++ web_dynamic/static/script/2-hbnb.js
@@ -15,3 +15,10 @@ $(document).ready(function () {
 	  $('div.amenities > h4').text(amenityNames);
 	});
       });
+      $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+	if (textStatus === 'success' && data.status === 'OK') {
+	  $('#api_status').addClass('available');
+	} else {
+	  $('#api_status').removeClass('available');
+	}
+      });
